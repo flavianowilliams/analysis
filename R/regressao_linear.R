3c13359206ac21aa82ed2c824c6f9441df21040a
flavianowilliams <flavianowilliams@gmail.com>
2023-09-12 16:35:53 -0300

    updated ggplot in regressao_linear script

--- R/regressao_linear.R
+++ R/regressao_linear.R
@@ -6,25 +6,26 @@ library(tidyverse)
 library(plotly)
 
 # adicione os valores para x e y
-x = c(10,20,30,40)
-y = c(10,20,30,40)
+x = c(10,20,20,40)
+y = c(10,20,25,40)
 
 df = data.frame(x=x, y=y)
 
 tibble(df)
 
 regressao = lm(y~x, data = df)
+summary(regressao)
 
 stats_df = summary(regressao)$coefficients
 
 A = stats_df["x", "Estimate"]
+B = stats_df["(Intercept)", "Estimate"]
 
 cat("O coeficiente de atrito encontrado foi", A)
 
 p = ggplot(data = df, aes(x, y))+
-  geom_point(size = 3, aes(color="Dados"))+
-  geom_smooth(method = "lm", se = FALSE, aes(color="y=Ax+B"))+
-  annotate("text", x=30, y=15, label=sprintf("Coeficiente de atrito: %1.2f", A))+
-  scale_color_manual(name="Cores", breaks = c("Dados", "y=Ax+B"), values = c("black", "red"))
+  geom_point(size = 2)+
+  geom_abline(intercept = B, slope = A, colour = "red")+
+  annotate("text", x=0.75*max(x), y = 0.4*max(y), label=sprintf("Coeficiente de atrito: %1.2f", A))
 
 ggplotly(p)
