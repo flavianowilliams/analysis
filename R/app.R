20c5c2e5ee8efee382f1d6cabf2a8e46ae417e0f
flavianowilliams <flavianowilliams@gmail.com>
2023-09-26 10:04:44 -0300

    update dashboard app

--- R/app.R
+++ R/app.R
@@ -1,33 +1,33 @@
 library(tidyverse)
 library(shiny)
+library(plotly)
 
 ui = fluidPage(
-  titlePanel("Results of the MD simulation of condensed systems"),
-  
+  titlePanel("Statistical analysis of physical problems"),
   sidebarLayout(
     sidebarPanel(helpText("Analysis is an open source platform dedicated
                           to data analysis of physical problems. For more information, visit ",
                  a("wiki documentation",href="https://github.com/flavianowilliams/analysis"),
                  " in the GitHub page."),
     fileInput("file","Upload your dataset",accept = ".csv"),
-    textInput("x", h3("Valores de x"), value = "x"),
-    textInput("y", h3("Valores de y"), value = "y"),
+    textInput("x", h4("Informe a coluna com os valores de x"), value = "X"),
+    textInput("y", h4("Informe a coluna com os valores de y"), value = "Y"),
     br(),
-    actionButton("apply", label="Apply"),
-    br(),
-    #    selectInput("variable",
-#                label = "Enter a variable",
-#                choices = list("Volume"="volume",
-#                               "Temperature"="temperature",
-#                               "Pressure"="pressure",
-#                               "Intramolecular energy"="intramolecular",
-#                               "Intermolecular energy"="intermolecular",
-#                               "Energy"="energy"),
-#                selected = "energy"),
-    downloadButton("download", label="Download")),
+    actionButton("apply", label="Apply")),
+#    br(),
+#    downloadButton("download", label="Download")),
     mainPanel(
-      textOutput("stats"),
-      plotOutput("plot")
+      navbarPage("Analysis",
+                 tabPanel("Dataframe",
+                          tableOutput("dataframe")
+                 ),
+                 tabPanel("Statistics",
+                          textOutput("stats")
+                 ),
+                 tabPanel("Plot",
+                            plotlyOutput("plot")
+    )
+      )
     )
   )
 )
@@ -45,49 +45,57 @@ server = function(input, output) {
     read.csv2(file$datapath)
   })
 
+  output$dataframe = renderTable({currentFile()})
+  
   observeEvent(input$apply, {
+
+    data = currentFile() %>%
+      mutate(x = get(input$x)) %>%
+      mutate(y = get(input$y))
     
-    x = input$x
-    y = input$y
-    regressao = lm(y~x, data = currentFile())
+    regressao = lm(y ~ x, data = data)
     summary(regressao)
     stats_df = summary(regressao)$coefficients
     
     v$A = stats_df["x", "Estimate"]
     v$B = stats_df["(Intercept)", "Estimate"]
-    
+
   })
   
   output$stats = renderText({
     
     if (is.null(v$A)) return()
-    paste("Coeficiente angular:", v$A)
-    
+  paste("Coeficiente angular:", v$A)
+
   })
 
     plotreact = eventReactive(input$apply, {
+      
     x = input$x
     y = input$y
-    ggplot(data=currentFile(),aes(x=x,y=y))+
-      geom_point()+
+    
+    p = ggplot(data=currentFile(),aes(x=get(x),y=get(y)))+
+    geom_point()+
       geom_abline(intercept = v$B, slope = v$A, colour = "red")+
-#      annotate("text", x=0.75*max(x), y = 0.4*max(y), label=sprintf("y = %1.2f", 1.1))
       xlab(x)+
       ylab(y)
+    
+    ggplotly(p)
+    
   })
   
-  output$plot = renderPlot({
+  output$plot = renderPlotly({
       if (is.null(v$A)) return()
       plotreact()
   })
 
-  output$download = downloadHandler(
-    filename = function() {
-      paste(input$x, "_", input$y, "-", Sys.Date(), ".pdf", sep = "")
-    },
-    content = function(file) {
-    ggsave(file,plot = plotreact())
-  })
+#  output$download = downloadHandler(
+#    filename = function() {
+#      paste(input$x, "_", input$y, "-", Sys.Date(), ".pdf", sep = "")
+#    },
+#    content = function(file) {
+#    ggsave(file,plot = plotreact())
+#  })
 
 }
 
